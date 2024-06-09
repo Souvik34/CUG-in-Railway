62c998d25386ca238b4bd269a49eb12c8eddfb9f
Tanay Pramanick <112556611+TanayPramanick@users.noreply.github.com>
2024-06-10 02:44:12 +0530

    Admin updated

--- CUG-client/src/Pages/Admin-dashboard/Admin.jsx
+++ CUG-client/src/Pages/Admin-dashboard/Admin.jsx
@@ -4,7 +4,44 @@ import { Outlet } from 'react-router-dom'
 const Admin = () => {
   return (
     <div>
-      Admin Home 
+      <nav className="w-full text-center bg-custom-gradient">
+        <h1 className="text-4xl font-semibold py-5 text-white">ADMIN PORTAL</h1>
+      </nav>
+      <div className="bg-gray-100 min-h-screen flex">
+        <div className="bg-gray-800 w-64 p-4">
+          <div>
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mb-2 border-solid border-2 w-56">
+            CUG DETAILS
+            </button>
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mb-2 border-solid border-2 w-56">
+              Add New CUG
+            </button>
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mb-2 border-solid border-2 w-56">
+              Allotment History
+            </button>
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mb-2 border-solid border-2 w-56">
+              Allocation-Wise Report
+            </button>
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mb-2 border-solid border-2 w-56">
+              CUG Status Report
+            </button>
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mb-2 border-solid border-2 w-56">
+              Active/De-Active Report
+            </button>
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mb-2 border-solid border-2 w-56">
+              Upload CUG Bill
+            </button>
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mb-2 border-solid border-2 w-56">
+              Upload New CUG Nos
+            </button>
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded border-solid border-2 w-56">
+              Provider
+            </button>
+          </div>
+        </div>
+        <div className="flex-1 p-4">
+        </div>
+      </div>
       <Outlet/>
     </div>
   )
