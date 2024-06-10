6fdcbc383fe8ba14741951922e64a60e75b92ba3
Souvik <souvik13109@gmail.com>
2024-06-10 18:11:22 +0530

    "Updated Home page styling and layout, and added background color to body in index.css"

--- CUG-client/src/Pages/Home/Home.jsx
+++ CUG-client/src/Pages/Home/Home.jsx
@@ -7,16 +7,16 @@ const Home = () => {
   return (
     <>
       <div className='items-center text-center justify-center h-full'>
-        <h1 className='text-5xl text-[#048DC8]  font-noto font-semibold m-10'>CLOSED USER GROUP</h1>
-        <div className='items-center text-center justify-center md:mt-10 p-6  md:flex md:space-x-[20%] space-y-8 '>
+        <h1 className='text-5xl text-[#048DC8]  font-noto font-bold m-10'>CLOSED USER GROUP</h1>
+        <div className='items-center text-center justify-center md:mt-10 p-6 flex flex-wrap gap-[30vh]'>
           {/* admin card */}
-          <div className='justify-center text-center items-center bg-[#F4F4F4] px-[70px] py-[30px] rounded-[15px_50px_30px] shadow-lg'>
+          <div className='justify-center text-center items-center bg-[#F4F4F4] px-[70px] py-[30px] rounded-[30px_5px_30px_5px] shadow-xl'>
             <img src="../../../public/images/Admin_logo.png" alt="Admin" />
-            <button className='bg-[#4E9DF2] hover:bg-blue-600 mt-5 px-6 py-3 rounded-md text-white text-xl fant-semibold'>ADMIN LOGIN</button>
+            <button className='bg-[#4E9DF2] hover:bg-blue-600 mt-5 px-6 py-3 rounded-md text-white text-xl font-semibold'>ADMIN LOGIN</button>
           </div>
 
           {/* dealer card */}
-          <div className='justify-center text-center items-center bg-[#F4F4F4] px-[70px] py-[30px] rounded-[15px_50px_30px] shadow-lg'>
+          <div className='justify-center text-center items-center bg-[#F4F4F4] px-[70px] py-[30px] rounded-[30px_5px_30px_5px] shadow-xl'>
           <img src="../../../public/images/Dealer_logo.png" alt="Dealer" />
             <button className='bg-[#4E9DF2] hover:bg-blue-600  mt-5 px-6 py-3 rounded-md text-white text-xl font-semibold'>DEALER LOGIN</button>
           </div>
