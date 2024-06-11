54b4015d71d33aedd735b243b49bb51fd432576a
deedhitidingal <98609979+deedhitidingal@users.noreply.github.com>
2024-06-11 15:31:17 +0530

    Update

--- CUG-client/src/Pages/Login/AdminLogin.jsx
+++ CUG-client/src/Pages/Login/AdminLogin.jsx
@@ -91,7 +91,9 @@ const AdminLogin = () => {
 
                 {/* for password */}
                 <label htmlFor="username">password</label>
-                <div>
+                <div className='flex'>
+                
+              
                 <input
                   type={visible?"text":"password"}
                   name="password"
@@ -110,7 +112,10 @@ const AdminLogin = () => {
 
               </div>
               <br />
+              <div className='justify-center'>
               <button type="submit" className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Login now</button>
+             
+              </div>
             </form>
           </div>
   </section>
