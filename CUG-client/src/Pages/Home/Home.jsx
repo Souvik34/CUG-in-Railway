89f310f56877870c9c95687d70de3fe8496e719b
Souvik <souvik13109@gmail.com>
2024-06-11 11:48:37 +0530

    "Updated CSS classes and styles for Dealer dashboard and Home page components"

--- CUG-client/src/Pages/Home/Home.jsx
+++ CUG-client/src/Pages/Home/Home.jsx
@@ -10,13 +10,13 @@ const Home = () => {
         <h1 className='text-5xl text-[#048DC8]  font-noto font-bold m-10'>CLOSED USER GROUP</h1>
         <div className='items-center text-center justify-center md:mt-10 p-6 flex flex-wrap gap-[30vh]'>
           {/* admin card */}
-          <div className='justify-center text-center items-center bg-[#F4F4F4] px-[70px] py-[30px] rounded-[30px_5px_30px_5px] shadow-xl'>
+          <div className='justify-center text-center items-center bg-[#F4F4F4] px-[70px] py-[30px] rounded-[15px_5px_15px_5px] shadow-xl border-t-[10px] border-purple-500'>
             <img src="../../../public/images/Admin_logo.png" alt="Admin" />
             <button className='bg-[#4E9DF2] hover:bg-blue-600 mt-5 px-6 py-3 rounded-md text-white text-xl font-semibold'>ADMIN LOGIN</button>
           </div>
 
           {/* dealer card */}
-          <div className='justify-center text-center items-center bg-[#F4F4F4] px-[70px] py-[30px] rounded-[30px_5px_30px_5px] shadow-xl'>
+          <div className='justify-center text-center items-center bg-[#F4F4F4] px-[70px] py-[30px] rounded-[15px_5px_15px_5px] shadow-xl border-t-[10px] border-blue-400'>
           <img src="../../../public/images/Dealer_logo.png" alt="Dealer" />
             <button className='bg-[#4E9DF2] hover:bg-blue-600  mt-5 px-6 py-3 rounded-md text-white text-xl font-semibold'>DEALER LOGIN</button>
           </div>
