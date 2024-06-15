ef275521e77a9bdc8880095e883291b7a5fd5d5d
deedhitidingal <98609979+deedhitidingal@users.noreply.github.com>
2024-06-15 11:58:49 +0530

    Welcome updated

--- CUG-client/src/Pages/Admin-dashboard/AdminWelcome.jsx
+++ CUG-client/src/Pages/Admin-dashboard/AdminWelcome.jsx
@@ -5,7 +5,7 @@ const AdminWelcome = () => {
   return (
     <div className="h-[550px] bg-gray-100 flex flex-col justify-center items-center p-6 w-full">
       <div className="bg-white shadow-lg rounded-lg p-5 max-w-lg w-full text-center justify-center items-center">
-        <img src="../../../public/images/Admin.svg" className='size-[50%] inline-flex' alt="" />
+        <img src="../../../public/images/Admin_sir.svg" className='size-[50%] inline-flex' alt="" />
         <h1 className="text-4xl font-bold  mb-4">Welcome, <span className='text-[#9D4EDD]'>Admin!</span></h1>
         <p className="text-gray-600 mb-8">
           Here is your dashboard where you can manage all aspects of the system.
