0e726c4d4fd6903a22ac5a99bd499671643acd9f
Souvik <souvik13109@gmail.com>
2024-06-19 01:09:19 +0530

    "Added Add_cug controller and router, and integrated with server.js"

--- CUG-server/router/add_cug-router.js
+++ CUG-server/router/add_cug-router.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router= express.Router();
-const addcugController=require("../controllers/add_cug-controller");
+const add_cugController = require("../controllers/add_cug-controller");
 
-router.route("/").post(addcugController.create);
+router.route("/").post(add_cugController.create);
+// router.route("/").get(add_cugController.getAll);
+// router.route("/:id").get(add_cugController.getById);
+
+// router.route("/:id").delete(add_cugController.delete);
+module.exports = router;
