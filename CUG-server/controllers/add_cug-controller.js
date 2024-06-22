e97a48e855e61506659917c291e00f27667e7a1d
SOURAV KARMAKAR <98399593+sourav8927@users.noreply.github.com>
2024-06-22 20:24:16 +0530

    Add cug Api updated

--- CUG-server/controllers/add_cug-controller.js
+++ CUG-server/controllers/add_cug-controller.js
@@ -1,25 +1,49 @@
 const Add_cug = require("../models/add_cug-model");
-    const create = async (req, res) => {
-        try {
-          console.log(req.body);
-          const response = req.body;
-          await Add_cug.create(response);
-          return res.status(201).json({ msg: "Add_cug created successfully" });
-        } catch (error) {
-          console.error(error);
-          res.status(400).json({ message: "Failed to create Add_cug" });
-        }
-      }; 
-      
-      const getAllData= async(req,res)=>{
-          try {
-            const allData= await Add_cug.find();
-            if(!allData){
-              res.status(404).json({message: "No data found"});
-            }
-            res.status(200).json({allData});
-          } catch (error) {
-            console.log(error);
-          }
-      }
-      module.exports = {create,getAllData};
+const create = async (req, res) => {
+  try {
+    console.log(req.body);
+    const {
+      cugNo,
+      empNo,
+      name,
+      designation,
+      division,
+      department,
+      billUnit,
+      allocation,
+      plan,
+    } = req.body;
+    const employeerExist = await Add_cug.findOne({cugNo,empNo});
+    if (employeerExistExist) {
+      return res.status(400).json({ message: "Employeer and Cug no  already exist" });
+    }
+    const userCreated = await Add_cug.create({
+      cugNo,
+      empNo,
+      name,
+      designation,
+      division,
+      department,
+      billUnit,
+      allocation,
+      plan,
+    });
+    return res.status(201).json({ msg: "Add_cug created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Failed to create Add_cug" });
+  }
+};
+
+const getAllData = async (req, res) => {
+  try {
+    const allData = await Add_cug.find();
+    if (!allData) {
+      res.status(404).json({ message: "No data found" });
+    }
+    res.status(200).json({ allData });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = { create, getAllData };
