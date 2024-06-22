8bff64080b700fbfc2913a988a88ba372aa2e11b
Souvik <souvik13109@gmail.com>
2024-06-22 21:04:50 +0530

    "Updated API endpoint URL in Add_New_CUG component, fixed typo in add_cug-controller, and added GET route to add_cug-router"

--- CUG-server/router/add_cug-router.js
+++ CUG-server/router/add_cug-router.js
@@ -3,8 +3,8 @@ const router= express.Router();
 const add_cugController = require("../controllers/add_cug-controller");
 
 router.route("/").post(add_cugController.create);
+router.route("/").get((req, res) => {
+  res.send('Server is running!');
+});
 router.route("/all_data").get(add_cugController.getAllData);
-// router.route("/:id").get(add_cugController.getById);
-
-// router.route("/:id").delete(add_cugController.delete);
 module.exports = router;
