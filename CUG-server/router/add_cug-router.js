0942d1b3f2386fc0610887bd4ddf3aef0a8b9b66
Souvik <souvik13109@gmail.com>
2024-06-24 13:00:22 +0530

    "Added 'Add New CUG' button to Dealer dashboard and corresponding route"

--- CUG-server/router/add_cug-router.js
+++ CUG-server/router/add_cug-router.js
@@ -4,6 +4,7 @@ const add_cugController = require("../controllers/add_cug-controller");
 const add_cugSchema = require("../validators/add_cug-validator");
 const validate = require("../middlewares/validate-middleware")
 
+
 router.route("/").post(validate(add_cugSchema), add_cugController.create);
 router.route("/all_data").get(add_cugController.getAllData);
 
